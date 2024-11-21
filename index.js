b96bdaa00fbd086219519dd8824f361e0fd0edcd
Michal Stasiak <michal.stasiak@protonmail.com>
2024-11-21 11:27:33 +0100

    fix and change name of last function

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// Napisz funkcję przetwarzającą tablicę obiektów osób. Funkcja powinna generować pseudonim na podstawie określonych zasad i dodawać go do każdego obiektu osoby, gdy jest to możliwe. Nie nadpisuj danych wejściowych.
-// Pobierz trzy ostatnie trzy litery imienia, odwróć ich kolejność i zapisz wynik
-// Weź pierwsze trzy litery nazwiska, odwróć ich kolejność i dodaj to do wyniku z poprzedniego punktu
-// Sformatuj połączony wynik tak, aby pseudonim zaczynał się od wielkiej litery, a reszta liter była mała.
-// Dodaj ten pseudonim jako nową właściwość do obiektu osoby.
-// Jeśli firstName lub lastName ma mniej niż trzy znaki (pomiń znaki białe) lub nie jest typu string, nie dodawaj właściwości pseudonimu dla tej osoby.
-
-function createPseudonim(table) {
-  table.forEach((person) => {});
-}
-
-function getChars(string, direction) {
-  let chars = [];
-  if (direction === firstName) {
-    chars = string.slice(0, 3);
-  } else chars = string.slice(-3);
-  return chars;
-}
-
 const entryTable = [
   {
     firstName: false,
@@ -44,3 +25,32 @@ const entryTable = [
     lastName: null,
   },
 ];
+// Napisz funkcję przetwarzającą tablicę obiektów osób. Funkcja powinna generować pseudonim na podstawie określonych zasad i dodawać go do każdego obiektu osoby, gdy jest to możliwe. Nie nadpisuj danych wejściowych.
+// Pobierz trzy ostatnie trzy litery imienia, odwróć ich kolejność i zapisz wynik
+// Weź pierwsze trzy litery nazwiska, odwróć ich kolejność i dodaj to do wyniku z poprzedniego punktu
+// Sformatuj połączony wynik tak, aby pseudonim zaczynał się od wielkiej litery, a reszta liter była mała.
+// Dodaj ten pseudonim jako nową właściwość do obiektu osoby.
+// Jeśli firstName lub lastName ma mniej niż trzy znaki (pomiń znaki białe) lub nie jest typu string, nie dodawaj właściwości pseudonimu dla tej osoby.
+
+function addNickname(table) {
+  table.forEach((person) => {});
+}
+
+function createNickname(object) {
+  const firstNameChars = object.firstName
+    .slice(-3)
+    .split("")
+    .reverse()
+    .join("");
+  const lastNameChars = object.lastName
+    .slice(0, 3)
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+  let nickname = lastNameChars + firstNameChars;
+  nickname = nickname.charAt(0).toUpperCase() + nickname.slice(1);
+  return nickname;
+}
+
+console.log(getChars(entryTable[1]));
